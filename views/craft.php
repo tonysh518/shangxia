f61c477c34c6b32d6991216207b2ff8c16102052
hdg <hdg1988@gmail.com>
2014-09-04 11:56:05 +0800

    fix

--- views/craft.php
+++ views/craft.php
@@ -54,7 +54,27 @@ include_once 'common/header.php';?>
 						<div class="knowhowpic  knowhowL"><img src="../SX/images/knowhowpic.jpg" width="100%" /></div>
 					</div>
 					<div class="knowhowitem cs-clear intoview-effect" data-effect="fadeup">
-						<div class="knowhowintro2  knowhowL"><p>COMBING <a href="#">read</a> </p></div>
+						<div class="knowhowintro2  knowhowL">
+							<p>COMBING <a href="#" data-a="craft-read">read</a> </p>
+							<textarea style="display:none;">
+								<div class="popshade"></div>
+								<div class="pop">
+									<div class="popclose" data-a="popclose"></div>
+									<!--  -->
+									<div class="popcon">
+										<h2>Construction</h2>
+										<div class="popcontxt">
+											<p>All Chinese furniture bases its construction on ancient architecture. The bridge, beam, post, pole and sparrow brace in architecture all have their equivalent within chair making, as the seat frame, edge, stretcher and brace are similar to elements in architecture.</p>
+											<p>Metal nails are never used when connecting components, instead, mortise and tenons are used to fit legs and waists together in waisted furniture, and also together with stretchers. </p>
+											<p>Ancient people used the expression “will not fall in eternity” to describe the strength of mortise and tenons. Mortise and tenons can endure pressure from several directions making the furniture as stable and enduring as Mount Tai. </p>
+											<p>In ancient times, due to the technical limit of carpentry, mortise and tenons were difficult to make and joints came loose easily. </p>
+											<p>Today, with the help of precise mechanisms,Master Gu has improved the accuracy of the joints by perfecting the quality of the mortise and tenons so that indeed they “will not fall in eternity”.</p>
+										</div>
+									</div>
+								</div>
+							</textarea>
+						</div>
+
 						<div class="knowhowpic  knowhowR"><img src="../SX/images/knowhowpic.jpg" width="100%" /></div>
 					</div>
 					<div class="knowhowitem cs-clear intoview-effect" data-effect="fadeup">
