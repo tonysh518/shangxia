e97781a02615478d881f18898b11426fa140b140
Jackey <jziwenchen@gmail.com>
2014-09-06 21:31:16 +0800

    about 页面

--- admin/protected/messages/en_us/strings.php
+++ admin/protected/messages/en_us/strings.php
@@ -1,5 +1,24 @@
 <?php 
 
 return array(
+    "about_brand_story_title" => "brand story<br>Renaissance of Chinese Art of Living",
+    "about_brand_story_desc" => "<p>SHANG XIA is a brand for art of living that promises a unique encounter with the heritage of Chinese design and craftsmanship.</p>".
+				"<p>The renowned designer Jiang Qiong Er established SHANG XIA with a mission to create a 21st century lifestyle founded on the finest of Chinese design traditions.</p>".
+				"<p>The SHANG XIA name is simple yet profound. It embodies the concept of “up” and “down” in the continuous flow of energy from past, present and future, transmitting the essence of Chinese culture and its sublime aesthetics.</p>".
+				"<p>Building on 5,000 years of unique inheritance, and in the spirit of dialogue between tradition and modernity, SHANG XIA integrates the warmth, balance and harmony of Chinese grace into all its products.</p>".
+				"<p>SHANG XIA’s collections are comprised of striking furniture, fine decorative objects, exquisite accessories and luxurious garments. Uniquely, SHANG XIA also creates an annual series of limited edition “cultural objects”. These collectibles convey messages of tradition, guardianship and remembrance.</p>".
+				"<p>With flair and exacting attention to detail, SHANG XIA preserves the historical beauty and craftsmanship born of Chinese ingenuity, to provide a simple, elegant 21st century aesthetic.</p>",
+    
+    "about_brand_founder_title" => "FOUNDER & ARTISTIC DIRECTOR OF SHANGXIA",
+    "about_brand_founter_desc" => 'As Artistic Director and CEO of SHANG XIA, Ms. Jiang Qiong Er is a Chinese contemporary designer of international reputation infusing the subtlety, beauty and heritage of Chinese culture. Her designs breathe elegance of innovation and imagination. Her international vision and open mind, along with her multi-cultural background, naturally allow her creativity to express itself; preserving and respecting tradition, both Eastern and Western.Jiang Qiong Er was introduced to traditional painting when she was only two and later became a student of famous painter Cheng Shi Fa and calligrapher Han Tian Hong. After graduation from Tong Ji University in Art &amp; Design, she went on to the Decorative Arts School in Paris to further her studies in furniture and interior design.',
+    "about_brand_emplee_subject" => 'HERITAGE & ENCOUNTER',
+    "about_brand_emplee_one" => 'Xiu Yin, the 90 year old grandmother From the Republic, to the war To the liberation of new China To the new century Says she, she has had four lives Today, she still radiantly smiles',
+    "about_brand_emplee_two" => "Hui Jie, the 65 year old mother From the countryside To the open reform To the frenzy of construction Her life bears the weight of three eras of time Today, she continues to lightly dance",
+    "about_brand_emplee_third" => "Qiong Er, the 33 year old daughter
+From educational reform
+To the booming economy
+To East-West cross cultural exchange
+She is a fortunate witness to and experiences two styles of life Today, she carries on the ambition
+She carries on the ideal",
     
 );
