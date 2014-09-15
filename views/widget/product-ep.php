3be3c42897ae680d2394dc1fce4b48a386c19335
Jackey <jziwenchen@gmail.com>
2014-09-16 00:59:23 +0800

    翻译

--- views/widget/product-ep.php
+++ views/widget/product-ep.php
@@ -6,26 +6,16 @@
 				<div class="productscom">
 					<div class="productsinfor cs-clear">
 						<div class="proinfortxt aboutinfortxt intoview-effect related-product" data-effect="fadeup" style='background-color: #edf3f3;'>
-              <h2>Thin and Beautiful <br> Rare Perfection</h2>
-              <p>
-It is obvious to any viewer that Master Lu has poured his heart and experience into creating “Tianlai” (“Echo”), the eggshell porcelain for “Shang Xia”. The design of the “Tianlai” bowl is simple in shape, but conveys brilliance and near transparency in a daily object, just as the Chinese saying goes; “Turn nothing into something”. 
-If to set the high standard of beauty for eggshell porcelain as thin, translucent 
-and made from fine ingredients, then to achieve that beauty is not easy. 
-At any moment the thinness can be pierced or the body deformed. </p>
+              <h2><?php echo Yii::t("strings", "Thin and Beautiful <br> Rare Perfection")?></h2>
+              <?php echo Yii::t("strings", "Thin and Beautiful <br> Rare Perfection Desc")?>
+              
 							<a href="#" data-a="show-pop" class="btn transition-wrap"><span class="transition">Read more<br><br>Read more</span></a>
 							<textarea style="display:none;">
-								<h2>Thin and Beautiful, Rare Perfection </h2>
+								<h2><?php echo Yii::t("strings", "Thin and Beautiful <br> Rare Perfection")?></h2>
 								<div class="popcontxt">
-                  <p>It is obvious to any viewer that Master Lu has poured his heart and experience into creating “Tianlai” (“Echo”), the eggshell porcelain for “Shang Xia”. The design of the “Tianlai” bowl is simple in shape, but conveys brilliance and near transparency in a daily object, just as the Chinese saying goes; “Turn nothing into something”. </p>
-                  <p>If to set the high standard of beauty for eggshell porcelain as thin, translucent and made from fine ingredients, then to achieve that beauty is not easy. At any moment the thinness can be pierced or the body deformed. In the “Tianlai“ collection, the eggshell is so thin that it breaks easily when made. Master Lu controlled the process from the very beginning, mixing the proportion of kaolin clay, quartz and feldspar and insuring only the proper clay mixture made it to be fired a second time at the glazing temperature of 1380 ℃. “Firing porcelain requires an understanding of clay and fire. You have to listen to their language,” said Master Lu. </p>
-                  <p>Each ”Tianlai” bowl is almost identical. The perfect combination of pulling, paring and trimming hundreds of times, guarantees the best quality in the final objects. The quality lies in the expert control of the hand, each object is like a breath of air, where upon firing, each object is seemingly as light as a drifting cloud</p>
-                  <p>As experienced and composed as Master Lu is, he never knows what will happen in the next kiln. There are always surprises when opening the kiln, there will be cast offs, wares with defects and on rare occasions, works of brilliance.
-</p>
-                <p>Eggshell porcelain wares are witness to the skill and craftsmanship of the master’s hands. In the purity and translucency of the white, the universe is mirrored, empty yet filled. 
-</p>
-</div>
+                  <?php echo Yii::t("strings", "Thin and Beautiful <br> Rare Perfection Desc")?>
+                </div>
 							</textarea>
-              
 						</div>
 						<div class="proinforpic intoview-effect" data-effect="fadeup" data-effect-delay="400">
 							<div class="slide">
