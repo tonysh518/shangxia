ee04d116d33b9c82f56b23378cc7c615b3fafcf0
Jackey <jziwenchen@gmail.com>
2014-09-17 16:27:18 +0800

    .

--- views/js/lp.base.js
+++ views/js/lp.base.js
@@ -2051,7 +2051,7 @@ LP.use(['jquery' ,'easing' , '../api'] , function( $ , easing , api ){
     // 多语言切换
     LP.action('chang-lang' , function(){
         LP.setCookie( "lang", $(this).data("lang") );
-        LP.reload();
+        window.location.href = "/";
         return false;
     });
 
