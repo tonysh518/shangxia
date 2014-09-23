c897511006ebd5b4867aa0d9374b0c7ee8f87f53
hdg <hdg1988@gmail.com>
2014-09-23 23:41:19 +0800

    fix

--- views/gift-conner.php
+++ views/gift-conner.php
@@ -4,7 +4,7 @@
 			<div class="detail cs-clear">
 				<div class="arrows arrows4 detailprev" data-a="page-prev"></div>
 				<div class=" detailcon ">
-					<h2 class="colllisticon">shang xia gift corner</h2>
+					<h2 class="colllisticon intoview-effect" data-effect="fadeup">shang xia gift corner</h2>
 					<p>The products below are available in out boutiques. They are great gifts for your beloved ones. <br />Please select the products you would be interested in, and give us your contact information. You will then receive a personal call from someone at Shang Xia who will help you bare your gift delivered</p>
 				</div>
 				<div class="arrows arrows4 detailnext" data-a="page-next"></div>
@@ -18,7 +18,7 @@
 					<ul class="piclist cs-clear">
             <?php $gifts = GiftContentAR::model()->getList();?>
             <?php foreach ($gifts as $gift): ?>
-              <li class="piclistitem ">
+              <li class="piclistitem intoview-effect" data-effect="fadeup">
                 <img src="<?php echo $gift->thumbnail?>" width="100%" />
                 <p><span class=""><?php echo $gift->title?></span></p>
                 <a href="#" class="btn transition-wrap"><i class="transition"><?php echo Yii::t("strings", "I Want To Buy")?><br/><br/><?php echo Yii::t("strings", "I Want To Buy")?></i></a>
