fb92df0e00d046ff4d737e80dbaf736eb6ce4c43
Jackey <jziwenchen@gmail.com>
2014-10-14 23:16:20 +0800

    .

--- views/admin/protected/models/BuyContentAR.php
+++ views/admin/protected/models/BuyContentAR.php
@@ -15,6 +15,42 @@ class BuyContentAR extends ContentAR {
     return parent::getFields();
   }
   
-  
+  public function getList($limit = FALSE, $offset = FALSE) {
+    global $language;
+    // key: {type}_{limit}_{offset}
+    $key = "{$language}_{$this->type}_{$limit}_{$offset}";
+    if (isset(self::$cached_list[$key])) {
+      return self::$cached_list[$key];
+    }
+    $query = new CDbCriteria();
+    
+    if ($limit) {
+      $query->limit = $limit;
+    }
+    if ($offset) {
+      $query->offset = $offset;
+    }
+    
+    $type = $this->type;
+    
+    if ($type) {
+      $query->addCondition("type=:type", $type);
+      $query->params[":type"] = $type;
+    }
+    
+    $query->order = "weight DESC, cdate DESC";
+    
+    $query->addCondition("status=:status");
+    $query->params[":status"] = self::STATUS_ENABLE;
+    
+    $rows = $this->findAll($query);
+    
+    // 缓存保存好的Get list
+    // key: {type}_{limit}_{offset}
+    $key = "{$this->type}_{$limit}_{$offset}";
+    self::$cached_list[$key] = $rows;
+    
+    return $rows;
+  }
 }
 
